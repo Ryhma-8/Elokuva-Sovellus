b409387e1eaa826655cf29baa1a815bdf49433d7
JuhoYlostalo <juho.ylostalo@gmail.com>
2025-09-10 17:42:44 +0300

    Alustava elokuva lista saatu staattisella hakusanalla

--- src/screens/App.jsx
+++ src/screens/App.jsx
@@ -1,11 +1,22 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import movieSearch from '../services/simpleMovieSearch'
+import MoviesList from '../components/HeroBlock/movieList'
 import '../css/App.css'
 
 function App() {
-
+  const [movies, setMovies] = useState([])
+  useEffect(() => {
+    async function getMovies(){
+      const result = await movieSearch("star wars")
+      setMovies(result)
+    }
+    getMovies()
+  },[])
+  
   return (
     <>
       <h3>test</h3>
+        <MoviesList movies={movies} />
     </>
   )
 }
