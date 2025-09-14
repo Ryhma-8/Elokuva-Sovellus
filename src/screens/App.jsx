1582768e63811515327e36416c05cf4ff88087f9
Joni Sirviö <t3sijo05@students.oamk.fi>
2025-09-14 18:27:02 +0300

    Initial commit for review components. Both are still in the works.

--- src/screens/App.jsx
+++ src/screens/App.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react'
 import '../css/App.css'
 import MovieBlock from '../components/MovieBlock'
+import ReviewCarousel from '../components/ReviewCarousel'
+import LeaveReview from '../components/LeaveReview'
 function App() {
   
   
@@ -8,6 +10,9 @@ function App() {
     <>
       <h3>test</h3>
       <MovieBlock movieId={122} />
+      <ReviewCarousel />
+      <LeaveReview />
+
     </>
   );
 }
