7a2a06cc3ebcd70216866346af60cb138588ac91
Joni Sirviö <t3sijo05@students.oamk.fi>
2025-09-14 11:03:25 +0300

    Initial moviepage commit. Contains the movieblock which shows information for a specific movie

--- src/screens/App.jsx
+++ src/screens/App.jsx
@@ -1,14 +1,15 @@
 import { useEffect, useState } from 'react'
 import '../css/App.css'
-
+import MovieBlock from '../components/MovieBlock'
 function App() {
   
   
   return (
     <>
       <h3>test</h3>
+      <MovieBlock movieId={122} />
     </>
-  )
+  );
 }
 
 export default App
