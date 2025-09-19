24c97bfc185a4b6cd9b2ff2cd52b185b6ff8d1b6
JuhoYlostalo <juho.ylostalo@gmail.com>
2025-09-19 18:20:04 +0300

    Vaihdettu user objektista yksinkertaisiksi kentiksi

--- src/screens/moviePage.jsx
+++ src/screens/moviePage.jsx
@@ -2,17 +2,17 @@ import React from "react";
 import MovieBlock from "../components/moviePageComponents/MovieBlock";
 import ReviewCarousel from "../components/moviePageComponents/ReviewCarousel";
 import LeaveReview from "../components/moviePageComponents/LeaveReview";
-
 import Header from "../components/header";
 import Footer from "../components/footer";
 
 const MoviePage = () => {
   return (
     <div>
+      <Header></Header>
         <MovieBlock movieId={121} />
         <ReviewCarousel />
         <LeaveReview />
-
+      <Footer></Footer>
     </div>
   );
 };
