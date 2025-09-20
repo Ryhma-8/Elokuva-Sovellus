103802e6a0f83c03b6490f0e23bd23b9e281aa49
JuhoYlostalo <juho.ylostalo@gmail.com>
2025-09-20 19:16:52 +0300

    Lisätty credentials salitty CORS:ssa. Muistutus että frontissa pitää tämä sama lisätä axios kutsuihin.

--- Backend/index.js
+++ Backend/index.js
@@ -11,7 +11,9 @@ const port = process.env.PORT
 const app = express();
 
 
-app.use(cors());
+app.use(cors({
+     credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser())
