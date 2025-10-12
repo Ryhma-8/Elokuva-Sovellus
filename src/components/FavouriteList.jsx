b0cf46d7549b450782ae1c7314d025bbeb8d0a1c
JuhoYlostalo <juho.ylostalo@gmail.com>
2025-10-12 16:45:38 +0300

    css korjauksia

--- src/components/FavouriteList.jsx
+++ src/components/FavouriteList.jsx
@@ -135,15 +135,15 @@ export default function FavouriteList({ userId, groupId, refreshTrigger }) {
                 </p>
               </div>
               </Link>
-              {!userId && isProfilePage || isGroupPage && (
-                <button
-                  className="remove-fav-button"
-                  onClick={() => handleRemove(movie.id)}
-                  title="remove from favourites"
-                >
-                  <Trash2 size={18} />
-                </button>
-              )}
+              {((isProfilePage && !userId) || isGroupPage) && (
+              <button
+                className="remove-fav-button"
+                onClick={() => handleRemove(movie.id)}
+                title="remove from favourites"
+              >
+                <Trash2 size={18} />
+              </button>
+            )}
             </li>
           ))}
         </ul>
