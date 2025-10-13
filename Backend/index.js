b488298dd0c2d0340c09ba4913fbe6b3b41b8e4c
JuhoYlostalo <juho.ylostalo@gmail.com>
2025-10-13 19:49:18 +0300

    fix

--- Backend/index.js
+++ Backend/index.js
@@ -36,7 +36,7 @@ app.use('/api/group', groupRouter);
 
 app.use(express.static(path.join(__dirname, "dist"))); 
 
-app.get("*", (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
